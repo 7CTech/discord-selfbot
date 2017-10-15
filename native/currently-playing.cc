14ae4e0abdd3a74615315930da716f7783866206
7CTech <ctech.exe@gmail.com>
2017-10-15 11:01:30 +1100

    native

---
+++ native/currently-playing.cc
@@ -0,0 +1,11 @@
+#include <node/node.h>
+#include "util.cc"
+
+namespace SelfBot {
+		namespace CurrentlyPlaying {
+				void getCurrentlyPlaying(const v8::FunctionCallbackInfo<v8::Value> &args) {
+						v8::Isolate *isolate = args.GetIsolate();
+						
+				}
+		} //namespace CurrentlyPlaying
+} //namespace SelfBot
---
+++ native/util.cc
@@ -0,0 +1,20 @@
+#include <string>
+#include <array>
+#include <memory>
+#include <cstdio>
+
+namespace SelfBot {
+		namespace Util {
+				/*https://stackoverflow.com/questions/478898/how-to-execute-a-command-and-get-output-of-command-within-c-using-posix*/
+				std::string shell(std::string command) {
+						std::array<char, 512> buf;
+						std::string res = "";
+						std::shared_ptr<FILE> pipe(popen(command.c_str(), "r"), pclose);
+						if (!pipe) throw std::runtime_error("popen() failed");
+						while (!feof(pipe.get())) {
+								if (fgets(buf.data(), 512, pipe.get()) != nullptr) res += buf.data();
+						}
+            return res;
+				}
+		}
+}
