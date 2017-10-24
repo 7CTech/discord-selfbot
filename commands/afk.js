5b34ccdae27e9419c86f90807cd486e04e53b0d5
7CTech <ctech.exe@gmail.com>
2017-10-24 21:14:24 +1100

    stuff

--- commands/afk.js
+++ commands/afk.js
@@ -1,5 +1,8 @@
 const Command = require("../command.js").Command;
+const util = require("../util.js");
+
+const argCount = 1;
 
 let afk = new Command("afk", (client, message) => {
-
-}, 1);
+    util.validateArgs(message.content, argCount);
+}, argCount);
--- commands/repo.js
+++ commands/repo.js
@@ -14,9 +14,8 @@ let repo = new Command("repo", (client, message) => {
     util.validateArgs(message.content, this.argCount);
     const defaults = {
         cwd: __dirname,
-        env: process.env;
-    }
+        env: process.env
+    };
     let origin = sshToHttpsGitURL(spawn("git", ["remote", "get-url", "origin"], )); //TODO: Figure out cwd code
     if (message.content.includes("@")) message.edit(sshToHttpsGitURL());
-
 }, 0);
