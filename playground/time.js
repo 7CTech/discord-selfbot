5aa440032d38c1b0aa96dbd28da866ccdbaff8d4
7CTech <ctech.exe@gmail.com>
2017-10-26 15:00:26 +1100

    time playrgound

--- playground/time.js
+++ playground/time.js
@@ -9,5 +9,5 @@ if (args.length > 2) printf("%s\n", "too many args");
 
 if (args[1] === "team") {
     get("http://www.thebluealliance.com/api/v3/team/frc" + args[2] + "?X-TBA-Auth-Key=" + global.secrets.tba_token);
-}   
+}
 
