edf5090865a43f0a250b67b16425dd948a92dc80
7CTech <ctech.exe@gmail.com>
2017-11-14 08:16:07 +1100

    delete more js

--- src/commands/kill.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const command_1 = require("../command");
-exports.kill = new command_1.Command("kill", (client, message) => {
-    message.delete();
-    client.destroy();
-}, 0);
