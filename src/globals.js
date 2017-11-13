58c27a94de971229a75ce2765070f84ef85a3de3
7CTech <ctech.exe@gmail.com>
2017-11-14 08:15:20 +1100

    remove js

--- src/globals.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let globalConfig = require("../config.json");
-function getConfig() {
-    return globalConfig;
-}
-exports.getConfig = getConfig;
--- src/native_module.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const native_module = require("../build/Release/native_module");
-function getCurrentlyPlaying() {
-    return native_module.getCurrentlyPlaying();
-}
-exports.getCurrentlyPlaying = getCurrentlyPlaying;
