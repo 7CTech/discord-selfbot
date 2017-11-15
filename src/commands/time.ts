8d86c1a6149ae44618ad43ce60490806d5a10821
7CTech <ctech.exe@gmail.com>
2017-11-16 07:39:47 +1100

    try do more time

--- src/commands/time.ts
+++ src/commands/time.ts
@@ -7,9 +7,28 @@ import {team} from "../team";
 import * as moment from "moment-timezone";
 import * as googleMaps from "@google/maps"
 
-interface ClientResponse {
+interface GeoCodeClientResponse {
     headers: Object,
-    json: Object,
+    json: {
+        geometry: {
+            location: {
+                lat: number,
+                lng: number
+            }
+        }
+    },
+    status: number
+}
+
+interface TimezoneClientResponse {
+    headers: Object
+    json: {
+        dstOffset: number,
+        rawOffset: number,
+        status: string,
+        timeZoneId: string,
+        timeZoneName: string
+    },
     status: number
 }
 
@@ -25,16 +44,51 @@ export let time:Command = new Command("time", async (client: Client, message: Me
     let location: string = "";
 
 
-    await request("https://thebluealliance.com/api/v3/team/frc" + team + "?X-TBA-Auth-Key=" + getTBAToken())
-        .on("data", (data => {
+    request("https://thebluealliance.com/api/v3/team/frc" + team + "?X-TBA-Auth-Key=" + getTBAToken())
+        .on("data",  (async data => {
             let teamData:team = JSON.parse(data.toString());
             location = teamData.country + "/" + teamData.city;
+            console.log(location);
         }));
     if (location === "") {
         message.edit("Invalid TBA location data");
         return;
     }
-    maps.geocode({address: location}, (error: any, response: ClientResponse) => {
 
+    let lng:number = 1000;
+    let lat:number = 1000;
+
+    await Promise.all([maps.geocode({address: location}, (error: any, response: GeoCodeClientResponse) => {
+        if (!error) {
+            lat = response.json.geometry.location.lat;
+            lng = response.json.geometry.location.lng;
+        } else {
+            message.edit("Failed to geocode");
+            return;
+        }
+    })]);
+
+    if (lng === 1000 || lat === 1000) {
+        message.edit("Failed to geocode");
+        return;
+    }
+
+    let tz:string = "";
+
+    maps.timezone({location: [lng, lat]}, (error: any, response: TimezoneClientResponse) => {
+        if (!error) tz = response.json.timeZoneId;
+        else {
+            message.edit("Failed to timezone");
+            return;
+        }
     });
+
+    if (tz === "") {
+        message.edit("Failed to timezone");
+        return;
+    }
+
+    let momentTz:moment.Moment = moment().tz(tz);
+
+    message.edit(momentTz.format("HH:mm" + " (" + momentTz.format("z") + ")"));
 }, 1);
