5412c635e71f5e0db0c6bce8f052e7127dce5bac
7CTech <ctech.exe@gmail.com>
2017-11-15 15:03:28 +1100

    config

--- src/commands/config.ts
+++ src/commands/config.ts
@@ -4,9 +4,14 @@ import {getConfig} from "../globals";
 import * as util from "../util"
 
 let configGet:Command = new Command("config", (client: Client, message: Message) => {
-    message.edit(getConfig()[util.getArgAtPosition(1)])
+    message.edit(getConfig()[util.getArgAtPosition(message.content, 1)])
 }, 1);
 
 let configSet:Command = new Command("config", (client: Client, message: Message) => {
+    let configItem = util.getArgAtPosition(message.content, 0);
+    if (!getConfig().hasOwnProperty(configItem)) message.edit("Invalid config option");
+    else {
+
+    }
 
 }, 2);
